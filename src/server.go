ed9e4a6d22af4f1f01b23af615cfc9f8ca59d72e
paxarpp <pavelpp1974@gmail.com>
2018-12-31 19:31:17 +0300

    step

--- src/server.go
+++ src/server.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
-	"log"
 )
 
-func homeRouterHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParceForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
+func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("form: ", r.Form)
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("scheme: ", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Println("v", v[0])
+
 	}
 	fmt.Fprintf(w, "hello, what are you?")
 
 }
 
 func main() {
-	http.HendleFunc("/", homeRouterHandler)
+	http.HandleFunc("/", HomeRouterHandler)
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
