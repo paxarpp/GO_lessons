01e8197f0905764bce6d17cc5f2d4586f0dc61cb
paxarpp <pavelpp1974@gmail.com>
2019-01-02 16:10:35 +0300

    serv

--- src/server.go
+++ src/server.go
@@ -1,121 +1,282 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
+	"time"
 
-	// This is `httprouter`. Ensure to install it first via `go get`.
-	"github.com/julienschmidt/httprouter"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
 )
 
-// We need a data store. For our purposes, a simple map
-// from string to string is completely sufficient.
-type store struct {
-	data map[string]string
-
-	// Handlers run concurrently, and maps are not thread-safe.
-	// This mutex is used to ensure that only one goroutine can update `data`.
-	m sync.RWMutex
-}
-
-var (
-	// We need a flag for setting the listening address.
-	// We set the default to port 8080, which is a common HTTP port
-	// for servers with local-only access.
-	addr = flag.String("addr", ":8080", "http service address")
-
-	// Now we create the data store.
-	s = store{
-		data: map[string]string{},
-		m:    sync.RWMutex{},
-	}
-)
-
-// ## main
 func main() {
-	// The main function starts by parsing the commandline.
-	flag.Parse()
+	stop := make(chan os.Signal, 1)
 
-	// Now we can create a new `httprouter` instance...
-	r := httprouter.New()
+	signal.Notify(stop, os.Interrupt)
 
-	// ...and add some routes.
-	// `httprouter` provides functions named after HTTP verbs.
-	// So to create a route for HTTP GET, we simply need to call the `GET` function
-	// and pass a route and a handler function.
-	// The first route is `/entry` followed by a key variable denoted by a leading colon.
-	// The handler function is set to `show`.
-	r.GET("/entry/:key", show)
+	handler := http.NewServeMux()
 
-	// We do the same for `/list`. Note that we use the same handler function here;
-	// we'll switch functionality within the `show` function based on the existence
-	// of a key variable.
-	r.GET("/list", show)
+	handler.HandleFunc("/book/", BasicAuth(Logger(HandleBook)))
 
-	// For updating, we need a PUT operation. We want to pass a key and a value to the URL,
-	// so we add two variables to the path. The handler function for this PUT operation
-	// is `update`.
-	r.PUT("/entry/:key/:value", update)
+	handler.HandleFunc("/books/", BasicAuth(Logger(HandleBooks)))
 
-	// Finally, we just have to start the http Server. We pass the listening address
-	// as well as our router instance.
-	err := http.ListenAndServe(*addr, r)
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        handler,          // if nil use default http.DefaultServeMux
+		ReadTimeout:    10 * time.Second, // max duration reading entire request
+		WriteTimeout:   10 * time.Second, // max timing write response
+		IdleTimeout:    15 * time.Second, // max time wait for the next request
+		MaxHeaderBytes: 1 << 20,          // 2^20 or 128kbytes
+	}
 
-	// For this demo, let's keep error handling simple.
-	// `log.Fatal` prints out an error message and exits the process.
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
+	log.Printf("Listening on http://%s\n", s.Addr)
+	log.Fatal(s.ListenAndServe())
+
+}
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		log.Printf("server [net/http] method [%s]  connection from [%v]", r.Method, r.RemoteAddr)
+
+		next.ServeHTTP(w, r)
 	}
 }
 
-// ## The handler functions
+func BasicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-// Let's implement the show function now. Typically, handler functions receive two parameters:
-//
-// * A Response Writer, and
-// * a Request object.
-//
-// `httprouter` handlers receive a third parameter of type `Params`.
-// This way, the handler function can access the key and value variables
-// that have been extracted from the incoming URL.
-func show(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 
-	// To access these parameters, we call the `ByName` method, passing the variable name that we chose when defining the route in `main`.
-	k := p.ByName("key")
+		if len(auth) != 2 || auth[0] != "Basic" {
+			http.Error(w, "authorization failed", http.StatusUnauthorized)
+			return
+		}
 
-	// The show function serves two purposes.
-	// If there is no key in the URL, it lists all entries of the data map.
-	if k == "" {
-		// Lock the store for reading.
-		s.m.RLock()
-		fmt.Fprintf(w, "Read list: %v", s.data)
-		s.m.RUnlock()
+		payload, _ := base64.StdEncoding.DecodeString(auth[1])
+		pair := strings.SplitN(string(payload), ":", 2)
+
+		if len(pair) != 2 || !validate(pair[0], pair[1]) {
+			http.Error(w, "authorization failed", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func validate(username, password string) bool {
+	if username == "test" && password == "test" { //Basic dGVzdDp0ZXN0
+		return true
+	}
+	return false
+}
+
+func HandleBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		HandleGetBooks(w, r)
+	}
+}
+
+func HandleGetBooks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	books, _ := json.Marshal(bookStore.GetBooks())
+
+	w.Write(books)
+}
+
+func HandleBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		HandleGetBook(w, r)
+
+	} else if r.Method == http.MethodPost {
+		HandleAddBook(w, r)
+
+	} else if r.Method == http.MethodPut {
+		HandleUpdateBook(w, r)
+
+	} else if r.Method == http.MethodDelete {
+		HandleDeleteBook(w, r)
+
+	} else {
+		HandleMethodIsNotAllowed(w, r)
+
+	}
+}
+
+func HandleMethodIsNotAllowed(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	msg, _ := json.Marshal(fmt.Sprintf("Method %s not allowed", r.Method))
+	w.Write(msg)
+}
+
+func HandleGetBook(w http.ResponseWriter, r *http.Request) {
+	bookid := strings.Replace(r.URL.Path, "/book/", "", 1)
+
+	book := bookStore.FindBookById(bookid)
+
+	if book == nil {
+		w.WriteHeader(http.StatusNotFound)
+		error, _ := json.Marshal(fmt.Sprintf("Book with id %s not found", bookid))
+
+		w.Write(error)
+
 		return
 	}
 
-	// If a key is given, the show function returns the corresponding value.
-	// It does so by simply printing to the ResponseWriter parameter, which
-	// is sufficient for our purposes.
-	s.m.RLock()
-	fmt.Fprintf(w, "Read entry: s.data[%s] = %s", k, s.data[k])
-	s.m.RUnlock()
+	w.WriteHeader(http.StatusOK)
+
+	bookidJson, _ := json.Marshal(book)
+
+	w.Write(bookidJson)
 }
 
-// The update function has the same signature as the show function.
-func update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func HandleAddBook(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
 
-	// Fetch key and value from the URL parameters.
-	k := p.ByName("key")
-	v := p.ByName("value")
+	var book Book
 
-	// We just need to either add or update the entry in the data map.
-	s.m.Lock()
-	s.data[k] = v
-	s.m.Unlock()
+	err := decoder.Decode(&book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error, _ := json.Marshal(fmt.Sprintf("Bad request. %v", err))
 
-	// Finally, we print the result to the ResponseWriter.
-	fmt.Fprintf(w, "Updated: s.data[%s] = %s", k, v)
+		w.Write(error)
+		return
+	}
+
+	err = bookStore.AddBook(book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error, _ := json.Marshal(fmt.Sprintf("Bad request. %v", err))
+
+		w.Write(error)
+		return
+	}
+
+	HandleGetBooks(w, r)
+}
+
+func HandleUpdateBook(w http.ResponseWriter, r *http.Request) {
+	bookid := strings.Replace(r.URL.Path, "/book/", "", 1)
+
+	decoder := json.NewDecoder(r.Body)
+
+	var book Book
+
+	err := decoder.Decode(&book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error, _ := json.Marshal(fmt.Sprintf("Bad request. %v", err))
+
+		w.Write(error)
+		return
+	}
+
+	book.Id = bookid
+
+	err = bookStore.SetBook(book)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		error, _ := json.Marshal(fmt.Sprintf("%v", err))
+
+		w.Write(error)
+
+		return
+	}
+
+	HandleGetBook(w, r)
+}
+
+func HandleDeleteBook(w http.ResponseWriter, r *http.Request) {
+	bookid := strings.Replace(r.URL.Path, "/book/", "", 1)
+	err := bookStore.DelBook(bookid)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		error, _ := json.Marshal(fmt.Sprintf("%v", err))
+
+		w.Write(error)
+
+		return
+	}
+
+	HandleGetBooks(w, r)
+}
+
+// BOOK
+
+type Book struct {
+	Id     string `json:"id"`
+	Author string `json:"author"`
+	Name   string `json:"name"`
+}
+
+type BookStore struct {
+	books []Book
+}
+
+var bookStore = BookStore{
+	books: make([]Book, 0),
+}
+
+func (s BookStore) GetBooks() []Book {
+	return s.books
+}
+
+func (s BookStore) FindBookById(id string) *Book {
+	for _, book := range s.books {
+		if book.Id == id {
+			return &book
+		}
+	}
+
+	return nil
+}
+
+func (s *BookStore) AddBook(book Book) error {
+	bk := s.FindBookById(book.Id)
+	if bk != nil {
+		return errors.New(fmt.Sprintf("Book with id %s already exists", book.Id))
+
+	}
+	s.books = append(s.books, book)
+
+	return nil
+}
+
+func (s *BookStore) SetBook(book Book) error {
+	for i, bk := range s.books {
+		if bk.Id == book.Id {
+
+			s.books[i] = book
+
+			return nil
+		}
+	}
+
+	return errors.New(fmt.Sprintf("There is no book with id %s", book.Id))
+}
+
+func (s *BookStore) DelBook(id string) error {
+	for i, bk := range s.books {
+		if bk.Id == id {
+
+			s.books = append(s.books[:i], s.books[i+1:]...)
+
+			return nil
+		}
+	}
+
+	return errors.New(fmt.Sprintf("There is no book with id %s", id))
 }
